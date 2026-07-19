87afca6cbd91403700cf2958fb80966a415770a0
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-07-19 13:45:22 -0500

    telemetry moved to app dir

--- src/coffeeplot/context.py
+++ src/coffeeplot/context.py
@@ -7,7 +7,7 @@ APP_NAME = "coffeeplot"
 APP_DIR = Path.home() / f".{APP_NAME}"
 EXPORT_DIR = APP_DIR / "export"
 DATABASE_DIR = APP_DIR / "database"
-DATABASE_DIR.mkdir(parents=True, exist_okay=True)
+DATABASE_DIR.mkdir(parents=True, exist_ok=True)
 
 config_mngr = DworshakConfig(path=APP_DIR / "config.json")
-config_mngr.set(service=APP_NAME,item="null","null")
+config_mngr.set(service=APP_NAME,item="null",value="null")
