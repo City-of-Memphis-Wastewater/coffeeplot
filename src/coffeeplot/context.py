4816cccf39743cebe61ac46b819d7c8614785982
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-07-19 14:30:49 -0400

    context, dworshak config

--- src/coffeeplot/context.py
+++ src/coffeeplot/context.py
@@ -1,1 +1,10 @@
-#
+# src/coffeeplot/context.py
+from __future__ import annotations
+from pathlib import Path
+from dworshak_config import DworshakConfig
+
+APP_NAME = "coffeeplot"
+APP_DIR = Path.home() / f".{APP_NAME}"
+APP_DIR.mkdir(parents=True, exist_okay=True)
+
+config_mngr = DworshakConfig(path=APP_DIR / "config.json")
