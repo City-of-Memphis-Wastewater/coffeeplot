aeb087b67a845be3bb56b9fb6c278259b687b92c
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-08-13 18:20:50 -0500

    ref description from pyproject

---
+++ src/coffeeplot/__main__.py
@@ -0,0 +1,6 @@
+# src/coffeeplot/__main__.py
+from __future__ import annotations
+from coffeeplot.cli import app
+
+if __name__ == "__main__":
+    app()
--- src/coffeeplot/context.py
+++ src/coffeeplot/context.py
@@ -3,11 +3,15 @@ from __future__ import annotations
 from pathlib import Path
 from dworshak_config import DworshakConfig
 
+from maxson_build_utils import PyProject
+pyproject = PyProject()
+
 APP_NAME = "coffeeplot"
 APP_DIR = Path.home() / f".{APP_NAME}"
 EXPORT_DIR = APP_DIR / "export"
 DATABASE_DIR = APP_DIR / "database"
 DATABASE_DIR.mkdir(parents=True, exist_ok=True)
+DESCRIPTION = pyproject.get("project","description")
 
 config_mngr = DworshakConfig(path=APP_DIR / "config.json")
 config_mngr.set(service=APP_NAME,item="null",value="null")
